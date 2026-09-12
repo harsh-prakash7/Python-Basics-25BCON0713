ec01cc810602923490c3c6b4ff828bfc3794564b
harsh-prakash7 <harshprakash31@gmail.com>
2026-09-12 09:04:20 +0530

    Update factorial.py
    fix

--- factorial.py
+++ factorial.py
@@ -1,8 +1,8 @@
-n = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
 fact = 1
 
-for i in range(1, n + 1):
+for i in range(1, num + 1):
     fact *= i
 
-print(f"Factorial of {n} is = {fact}")
+print(f"Factorial of {num} is = {fact}")
